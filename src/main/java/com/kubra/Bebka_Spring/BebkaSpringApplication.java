5298956472a4b0beb0f260928262658404e4cef4
Kübra Dönmez <kubradonmezz@outlook.com.tr>
2025-08-20 06:08:02 +0300

    Spring Boot project init

---
+++ src/main/java/com/kubra/Bebka_Spring/BebkaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kubra.Bebka_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebkaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebkaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubra/Bebka_Spring/BebkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubra.Bebka_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BebkaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
